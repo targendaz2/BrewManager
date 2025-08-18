80637cd1ae4ac26a12d5429cce47b97daf05462a
David Rosenberg <targendaz2@me.com>
2025-08-18 00:39:28 -0400

    Initial Commit

---
+++ BrewManager/BrewManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewManagerApp.swift
+//  BrewManager
+//
+//  Created by David Rosenberg on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct BrewManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrewManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrewManager
+//
+//  Created by David Rosenberg on 8/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
